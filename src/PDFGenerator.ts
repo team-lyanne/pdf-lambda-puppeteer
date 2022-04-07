178df244e63b397a44431db7c83b2c6de04c7b84
Louis Sommer <louis.sommer@gmail.com>
2022-04-07 23:32:57 +0200

    renamed routes

--- src/PDFGenerator.ts
+++ src/PDFGenerator.ts
@@ -9,41 +9,40 @@ export class PDFGenerator {
    * @param {any} event - The object that comes for lambda which includes the http's attributes
    * @returns {Array<any>} array of Structure Instructions
    */
-  static getPDF: GeneratorFunction = async (event) => {
+  static buildReport: GeneratorFunction = async (event) => {
     try {
       // Stubbing values for now
-      console.log(event.body);
-      event.reportFields = {
-        building_type: "Immeuble en copropriété",
-        date: "23/02/1990",
-        address: "15 rue yves toudic",
-        syndic_name: "Toto",
-        syndic_address: "Mich 75010 Paris",
-        recent_building: "NON",
-        leakage_test: "OUI",
-        origin_found: "OUI",
-        repaired: "OUI",
-        craftman_responsibility: "OUI",
-        craftman_reason: "OUI",
-        home_usage_a: "OUI",
-        termination_a: "OUI",
-        seasonal_furnished_a: "OUI",
-        damage_a: "OUI",
-        category_a: "Syndic",
-        owner_occupant_a: "OUI",
-        termination_b: "NON",
-        seasonal_furnished_b: "NON",
-        home_usage_b: "NON",
-        damage_b: "NON",
-        category_b: "Gérant de l'immeuble",
-        owner_occupant_b: "NON",
-        leakage_cause: "Fuite sur canalisation",
-        pipe_owner: "Commune",
-        pipe_role: "Alimentation",
-        pipe_accessibility: "Accessible",
-        infiltrations: "Façade, Fenêtre ou porte-fenêtre, Joint d'étanchéité (sanitaire ou carrelage)"
-      }
-      const attributes = mapReportFields(event.reportFields);
+      // event.reportFields = {
+      //   building_type: "Immeuble en copropriété",
+      //   date: "23/02/1990",
+      //   address: "15 rue yves toudic",
+      //   syndic_name: "Toto",
+      //   syndic_address: "Mich 75010 Paris",
+      //   recent_building: "NON",
+      //   leakage_test: "OUI",
+      //   origin_found: "OUI",
+      //   repaired: "OUI",
+      //   craftman_responsibility: "OUI",
+      //   craftman_reason: "OUI",
+      //   home_usage_a: "OUI",
+      //   termination_a: "OUI",
+      //   seasonal_furnished_a: "OUI",
+      //   damage_a: "OUI",
+      //   category_a: "Syndic",
+      //   owner_occupant_a: "OUI",
+      //   termination_b: "NON",
+      //   seasonal_furnished_b: "NON",
+      //   home_usage_b: "NON",
+      //   damage_b: "NON",
+      //   category_b: "Gérant de l'immeuble",
+      //   owner_occupant_b: "NON",
+      //   leakage_cause: "Fuite sur canalisation",
+      //   pipe_owner: "Commune",
+      //   pipe_role: "Alimentation",
+      //   pipe_accessibility: "Accessible",
+      //   infiltrations: "Façade, Fenêtre ou porte-fenêtre, Joint d'étanchéité (sanitaire ou carrelage)"
+      // }
+      const attributes = mapReportFields(event.body);
       const html = waterDamageTemplate(attributes);
       const options = {
         path: process.argv[3],
